90912dee967d001cb4ac07d5c86de00b9b9abf2a
Alexander <alexandrivantsov.ios@gmail.com>
2025-10-30 12:04:53 +0300

    v1.0.85: Define HAVE_MODULE_POW to enable PoW implementation and avoid redefinition conflicts

--- Package.swift
+++ Package.swift
@@ -160,6 +160,8 @@ let package = Package(
                 .define("TOR_UNIT_TESTS", to: "0"),
                 // Platform constants
                 .define("CHAR_BIT", to: "8"),
+                // Enable PoW module (equix support for hidden service DoS protection)
+                .define("HAVE_MODULE_POW", to: "1"),
                 // Force local includes to take precedence (shim for openssl/asn1_mac.h)
                 .unsafeFlags(["-I", "Sources/Tor/include"]),
                 // OpenSSL 1.1+/3.x APIs are available in our vendored OpenSSL
