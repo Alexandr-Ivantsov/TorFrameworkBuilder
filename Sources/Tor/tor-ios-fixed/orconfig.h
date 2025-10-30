e06eb76792c4558eb913497fad738ef2b3700b40
Alexander <alexandrivantsov.ios@gmail.com>
2025-10-30 07:50:15 +0300

    v1.0.73: Define HAVE_STRING_H and HAVE_UNISTD_H for process_unix.c

--- Sources/Tor/tor-ios-fixed/orconfig.h
+++ Sources/Tor/tor-ios-fixed/orconfig.h
@@ -135,6 +135,8 @@
 #define HAVE_SYS_UN_H 1
 #define HAVE_SIGNAL_H 1
 #define HAVE_ERRNO_H 1
+#define HAVE_STRING_H 1
+#define HAVE_UNISTD_H 1
 #define HAVE_SYS_UTIME_H 0
 #define HAVE_SYS_WAIT_H 1
 #define HAVE_TIME_H 1
